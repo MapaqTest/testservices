72dc4ab252e43d21a0805b497714b0b61ea23b92
Martin Leclerc <martin.leclerc@mapaq.gouv.qc.ca>
2018-03-22 17:23:41 -0400

    Service qui appelle le service HelloWorld

---
+++ Poc.AppelerService/Poc.AppelerService/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Poc.AppelerService.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            //return new string[] { "value1", "value2" };
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://helloworld-testservices.193b.starter-ca-central-1.openshiftapps.com");
+            HttpResponseMessage reponse = await client.GetAsync("api/Values");
+            return "Réponse du service : " + await reponse.Content.ReadAsStringAsync();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
